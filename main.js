35e5fcebd2034e293a865bf05bfae02de04e48be
Eric <Ericrius1@gmail.com>
2014-07-27 13:46:35 +0200

    shnur

--- main.js
+++ main.js
@@ -2,12 +2,11 @@ document.addEventListener( 'mousedown', mousedown, false );
 var height = window.innerHeight;
 var evens = [2,4,6,8,10];
 var odds = [1,3,5,7,9];
-var numElement = document.getElementById('number');
+var numberElement = document.getElementById('number');
 function mousedown(event){
-  if(event.clientY < window.innerHeight/2)
-    numElement.innerText = _.sample(evens, 1)
-  if(event.clientY > window.innerHeight/2)
-    numElement.innerText = _.sample(odds, 1)
-    ;
+  numberElement.innerText = "Generating..."
+  setTimeout(function(){
+    numberElement.innerText= event.clientY < window.innerHeight/2 ? _.sample(evens) : _.sample(odds);
+  }, 1100)
 }
 
